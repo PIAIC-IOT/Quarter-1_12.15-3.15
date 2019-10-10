90f76349d7e15ed861777d8c4864a6eceff781d7
imran82ali <code.imranali@gmail.com>
2019-10-10 11:47:23 +0500

    a

--- aug18/target/debug/aug18.d
+++
@@ -1,1 +0,0 @@
-/home/imran82ali/class/aug18/target/debug/aug18: /home/imran82ali/class/aug18/src/main.rs
--- aug18/target/debug/deps/aug18-71d6258a52a4dfa2.d
+++
@@ -1,5 +0,0 @@
-/home/imran82ali/class/aug18/target/debug/deps/aug18-71d6258a52a4dfa2: src/main.rs
-
-/home/imran82ali/class/aug18/target/debug/deps/aug18-71d6258a52a4dfa2.d: src/main.rs
-
-src/main.rs:
